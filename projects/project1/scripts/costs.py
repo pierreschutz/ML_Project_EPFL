9c7ec6412b4a29d1240f6f9390e51a325ee27c4b
Maxence Jouve <jouvemax@uw.edu>
2019-10-01 15:00:25 +0200

    added costs.py

---
+++ projects/project1/scripts/costs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Function used to compute the loss."""
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE / MAE
+    # ***************************************************
+    e = y - tx @ w
+    
+    return 1/(2*float(len(y))) * e @ np.transpose(e)
