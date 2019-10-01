031126f9cd2b619cb8b7f9f7fa4cd2b1e59a455b
Maxence Jouve <jouvemax@uw.edu>
2019-10-01 17:07:19 +0200

    solved almost everything

--- projects/project1/scripts/gradient_descent.py
+++ projects/project1/scripts/gradient_descent.py
@@ -8,31 +8,31 @@ def compute_gradient(y, tx, w):
     # TODO: compute gradient and loss
     # ***************************************************
 	N = len(tx)
-    return - 1/float(N) * np.transpose(tx) @ (y - tx @ w)
+	return - 1/float(N) * np.transpose(tx) @ (y - tx @ w)
 
 
 def gradient_descent(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
+	ws = [initial_w]
+	losses = []
+	w = initial_w
+	for n_iter in range(max_iters):
         # ***************************************************
         # INSERT YOUR CODE HERE
         # TODO: compute gradient and loss
         # ***************************************************
-        gradient = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w)
+		gradient = compute_gradient(y, tx, w)
+		loss = compute_loss(y, tx, w)
         # ***************************************************
         # INSERT YOUR CODE HERE
         # TODO: update w by gradient
         # ***************************************************
-        w = w - gamma * gradient
+		w = w - gamma * gradient
         # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+		ws.append(w)
+		losses.append(loss)
+		print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
-    return losses, ws
+	return losses, ws
